b17e973e95f45544e041ef2ae31836da7b4264a5
Florhaidee <florhaideedev@gmail.com>
2020-08-06 07:56:48 -0600

    created sessions, and  added botton to logout

--- server.js
+++ server.js
@@ -2,15 +2,33 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 //setup handlebars as template engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+//setup an Express.js session and connects the session to our Sequelize database
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-//setup handlebard template engine
+
+//use handlebard template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+//use session
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
